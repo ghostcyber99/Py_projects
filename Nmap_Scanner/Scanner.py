d10ce8d377c47fb4c7e79c3091cb2e27c2cf9a2d
ghostcyber99 <ericozeigbei@gmail.com>
2022-02-15 15:53:37 +0100

    nmap scanner first push

--- Nmap_Scanner/Scanner.py
+++ Nmap_Scanner/Scanner.py
@@ -14,6 +14,39 @@ type(ip_addr)
 resp = input("""\nPlease enter the type of scan you want to run
                 1)SYN ACK Scan
                 2)UDP Scan
-                3)Comprehensive  """)
+                3)Comprehensive  \n """)
 
 print(f"you have selected option: {resp}")
+
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v sS')
+    print(scanner.scaninfo())
+    print(("Ip Status: ", scanner[ip_addr].state)) #display the state of the ip 
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys()) #display all the open ports 
+elif resp == '2':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v sU')
+    print(scanner.scaninfo())
+    print(("Ip Status: ", scanner[ip_addr].state)) #display the state of the ip 
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['udp'].keys()) #display all the open ports 
+elif resp == '3':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print(("Ip Status: ", scanner[ip_addr].state)) #display the state of the ip 
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys()) #display all the open ports 
+elif resp >= '4':
+    print("please enter a valid option")
+
+
+
+     
+
+
+     
+
+
