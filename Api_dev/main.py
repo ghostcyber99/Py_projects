7ace9fd83d9743a6c7500336d061eb98faf873e2
ghostcyber99 <ericozeigbei@gmail.com>
2022-02-19 16:22:35 +0100

    CRUD dev done

--- Api_dev/main.py
+++ Api_dev/main.py
@@ -1,3 +1,4 @@
+from operator import index
 from turtle import pos
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
@@ -22,6 +23,11 @@ def find_post(id):
         if p["id"] == id:
             return p 
 
+def find_index_post(id):
+    for i , p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
 #get method 
 @app.get("/")
 def read_root():
@@ -32,9 +38,9 @@ def read_root():
 def get_posts():
     return{"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):   
-    post_dict = post.dict() #creatig a brand new post from the pydantic dictionary 
+    post_dict = post.dict() #creatig a brand new post from the python dictionary 
     post_dict['id'] = randrange(0, 10000000) #assign the id in the dict with a random number 
     my_posts.append(post_dict) #converting pydantic model to a dictionary and appeding it to an array
     return{"data": post_dict}
@@ -47,3 +53,28 @@ def get_post(id: int, response: Response):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
         detail=f"post with id: {id} was not found ")
     return{"post_details": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    #when id is not correct
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"post with id: {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    #find the index
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"post with id: {id} does not exist")
+    #converting to a python dictionary 
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return{"data": post_dict}
