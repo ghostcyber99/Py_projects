c20a48a94141c78abf78e1950a75c30620f75b0b
ghostcyber99 <ericozeigbei@gmail.com>
2022-03-14 17:24:40 +0100

    word counter app

--- Backend-django/mybackend/mybackend/myapp/urls.py
+++ Backend-django/mybackend/mybackend/myapp/urls.py
@@ -1,7 +1,10 @@
+from typing import Counter
+from unicodedata import name
 from django import views
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('counter', views.counter, name='counter')
 ]
